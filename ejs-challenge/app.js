b7fbc7e361e8c15b6cc76f9f71aca802ebaea240
Ashley Lee <ashleyhayjin@iMac.fios-router.home>
2021-04-25 17:23:54 -0500

    Routes and Mongoose Implemented

--- ejs-challenge/app.js
+++ ejs-challenge/app.js
@@ -3,6 +3,7 @@ const _ = require('lodash')
 const express = require("express");
 const bodyParser = require("body-parser");
 const ejs = require("ejs");
+const mongoose = require("mongoose");
 
 const homeStartingContent = "Lacus vel facilisis volutpat est velit egestas dui id ornare. Semper auctor neque vitae tempus quam. Sit amet cursus sit amet dictum sit amet justo. Viverra tellus in hac habitasse. Imperdiet proin fermentum leo vel orci porta. Donec ultrices tincidunt arcu non sodales neque sodales ut. Mattis molestie a iaculis at erat pellentesque adipiscing. Magnis dis parturient montes nascetur ridiculus mus mauris vitae ultricies. Adipiscing elit ut aliquam purus sit amet luctus venenatis lectus. Ultrices vitae auctor eu augue ut lectus arcu bibendum at. Odio euismod lacinia at quis risus sed vulputate odio ut. Cursus mattis molestie a iaculis at erat pellentesque adipiscing.";
 const aboutContent = "Hac habitasse platea dictumst vestibulum rhoncus est pellentesque. Dictumst vestibulum rhoncus est pellentesque elit ullamcorper. Non diam phasellus vestibulum lorem sed. Platea dictumst quisque sagittis purus sit. Egestas sed sed risus pretium quam vulputate dignissim suspendisse. Mauris in aliquam sem fringilla. Semper risus in hendrerit gravida rutrum quisque non tellus orci. Amet massa vitae tortor condimentum lacinia quis vel eros. Enim ut tellus elementum sagittis vitae. Mauris ultrices eros in cursus turpis massa tincidunt dui.";
@@ -10,16 +11,29 @@ const contactContent = "Scelerisque eleifend donec pretium vulputate sapien. Rho
 
 const app = express();
 
-let allPosts=[];
+// let allPosts=[];
 
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.static("public"));
 app.set('view engine', 'ejs');
 
+mongoose.connect("mongodb://localhost:27017/blogDB", {useNewUrlParser: true});
+
+const postSchema = {
+  title: String,
+  content: String
+};
+
+const Post = mongoose.model("Post", postSchema);
+
 
 app.get('/', (req,res) => {
-  res.render("home",
-  {startingContent: homeStartingContent, allPosts: allPosts});
+  Post.find({}, (err, posts) => {
+    console.log("posts", posts);
+    res.render("home",
+    {startingContent: homeStartingContent, posts: posts});
+  });
+
 })
 
 app.get('/about', (req,res) => {
@@ -33,31 +47,34 @@ app.get('/contact', (req,res) => {
   allPosts: allPosts} );
 })
 
-app.get("/posts/:id", (req,res) => {
-  let requestedID = _.lowerCase(req.params.id);
-  console.log("requestedID" , requestedID)
-  allPosts.forEach(function(allPost){
-    const storedID = _.lowerCase(allPost.postTitle);
-    if (requestedID === storedID){
+app.get("/posts/:postId", (req,res) => {
+  let requestedID = _.lowerCase(req.params.postId);
+  Post.findOne({_id: requestedID},function(allPost){
       res.render("post", {
-        title: allPost.postTitle,
-        content: allPost.postBody
+        title: allPost.title,
+        content: allPost.content
       });
-    }
+    })
   });
-})
 
 app.get("/compose", (req,res) => {
   res.render("compose");
 })
 
 app.post('/compose', (req,res) => {
-  const post = {
-    postTitle : req.body.postTitle,
-    postBody: req.body.postBody
-  };
-  allPosts.push(post)
-  res.redirect("/");
+  const post = new Post ({
+
+    title: req.body.title,
+ 
+    content: req.body.content
+ 
+  }) 
+  post.save(function(err){
+    if (!err){
+      res.redirect("/");
+
+    }
+  });
 })
 
 app.listen(3000, function() {
